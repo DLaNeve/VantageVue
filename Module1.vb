b63b9e0b548eabcdf852045ac494a9fa8d766ca4
Don LaNeve <don.laneve@gmail.com>
2014-12-09 11:59:23 -0500

    Barometer data now being received from VantageVue

--- Module1.vb
+++ Module1.vb
@@ -1,21 +1,20 @@
 ﻿Imports System.Collections.Specialized
-
 Module VantageVue
-
     Private Property GetBarometerData_V As Single
 
     Sub Main()
         Dim msgs As StringCollection = New StringCollection
         Open_USB_Port()
-
-        LoadCurrentVantageData_V()
-        msgs.Add(Create_Wind_msg())
-        msgs.Add(Create_Temp_msg())
-        msgs.Add(Create_Bar_msg())
-        msgs.Add(Create_Humidity_msg())
-        Broadcast_MSG(msgs)
-
-        msgs.Clear()
+        Do Until True = False
+            LoadCurrentVantageData_V()
+            msgs.Add(Create_Wind_msg())
+            msgs.Add(Create_Temp_msg())
+            msgs.Add(Create_Bar_msg())
+            msgs.Add(Create_Humidity_msg())
+            Broadcast_MSG(msgs)
+            msgs.Clear()
+            System.Threading.Thread.Sleep(3000)
+        Loop
 
     End Sub
 
@@ -47,7 +46,6 @@ Module VantageVue
 
         windspeed = GetWindSpeed_V()
         windDir = GetWindDir_V()
-        'windmsg = "$WIVWR,30.1,R,15.3,N,,,," & vbCrLf  a different method that also works
         windmsg = "$WIVWR," & windDir & ",R," & windspeed & ",N,,,," & vbCrLf
         Console.WriteLine(windmsg)
         Create_Wind_msg = windmsg
@@ -57,8 +55,14 @@ Module VantageVue
         Dim temp As Single
         Dim TempMsg As String
 
-        temp = GetOutsideTemp_V()
-        TempMsg = "$WIMTA," & temp & vbCrLf
+        '$IIXDR,P,1.02481,B,Barometer*0D
+        '$IIXDR,C,19.52,C,TempAir*3D
+        '$IIMTA,19.52,C*1E
+
+
+        temp = GetInsideTemp_V()
+        TempMsg = "$WIMTA," & temp & ",C" & vbCrLf
+        'TempMsg = "$WIXDR" & ",c," & temp & ",C1C" & vbCrLf
         Console.WriteLine(TempMsg)
         Create_Temp_msg = TempMsg
     End Function
@@ -66,42 +70,32 @@ Module VantageVue
         Dim bar As Short
         Dim barMsg As String
 
-        bar = GetBarometerData_V()
-        barMsg = "$WIMMB," & bar & vbCrLf
+        bar = GetBarometer_V()
+        barMsg = "$WIMMB," & bar & ",,,B" & vbCrLf
         Console.WriteLine(barMsg)
         Create_Bar_msg = barMsg
     End Function
     Function Create_Humidity_msg() As String
-        Dim Humidity As Short
+        Dim Humidity As Single
         Dim Humidity_msg As String
 
-        Humidity = GetOutsideHumidity_V()
+        Humidity = GetInsideHumidity_V()
         Humidity_msg = "$WIMHU," & Humidity & vbCrLf
         Console.WriteLine(Humidity_msg)
         Create_Humidity_msg = Humidity_msg
     End Function
     Sub Broadcast_MSG(msgs As StringCollection)
-        Dim broadcast As IPAddress = IPAddress.Parse("192.168.0.37")
-        Dim ep As New IPEndPoint(broadcast, 10110)
-        'Dim ep As New IPEndPoint(IPAddress.Loopback,10110)
-
-
+        ' Dim broadcast As IPAddress = IPAddress.Parse("192.168.0.37")
+        ' Dim ep As New IPEndPoint(broadcast, 10110)
+        Dim ep As New IPEndPoint(IPAddress.Loopback, 10110)
         Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
         socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, True)
         ' socket.Bind(ep)
-
         Dim sendbuf As Byte() = Encoding.ASCII.GetBytes(msgs(0))
-
-
         For Each msg As String In msgs
             sendbuf = Encoding.ASCII.GetBytes(msg)
             socket.SendTo(sendbuf, ep)
-
-
         Next
-
     End Sub
-
-   
 
 End Module
